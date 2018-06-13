037bfc614f471709a6ab9bdfca44dd424c9eb5d4
“Serena <serenahathi@hotmail.com>
2018-06-13 15:14:28 +0200

    Add API credentials as environmental variables

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 let express = require('express');
 let router = express.Router();
 let superagent = require("superagent");
-let apiKey = "9da31cd4c5408dfe55c830266d80de19c98830e1da9f24313"
 let request = require("request");
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index')
-  request("https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=" + apiKey, function(err, res, body) {
-  console.log(body.id);
-});
+  request("https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=" + process.env.API_KEY, function (err, res, body) {
+    let result = JSON.parse(body);
+    let word = result.word;
+    let firstDefinition = result.definitions[0].text;
+    let secondDefinition = result.definitions[1].text;
+  });
 });
 
 module.exports = router;

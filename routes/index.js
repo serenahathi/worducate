4a21580bd76a101f6575d3e6de142738ed87f602
Serena Hathi <hathi.serena@gmail.com>
2018-06-13 11:52:13 +0200

    test: practice mocking API call to GitHub

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let request = require("superagent");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+let getUserFollowers = function(username, callback) {
+  request
+    .get(`https://api.github.com/users/${username}/followers`)
+    .end(function(err, res) {
+      if (!err) {
+        let users = res.body.map(function(user) {
+          return user.login;
+        });
+        callback(null, users);
+      } else {
+        callback("Error occured!");
+      }
+    });
+};
+module.exports.getUserFollowers = getUserFollowers;
+// module.exports = router;
---
+++ test/indexSpec.js
@@ -0,0 +1,34 @@
+let expect = require("chai").expect;
+let nock = require("nock");
+let getUserFollowers = require("../routes/index").getUserFollowers
+let response = require("./response");
+
+describe("GET followers", function(){
+
+  beforeEach(function() {
+    nock('https://api.github.com')
+    .get("/users/octocat/followers")
+    .reply(200,response);
+  });
+
+  it("returns user followers", function(done){
+    this.timeout(3000)
+    // if the test takes longer than 3 scnds it will fail
+    
+    getUserFollowers("octocat", function(err, followers){
+      // it should return an array object
+      expect(Array.isArray(followers)).to.equal(true);
+      // there should be more than 1 element in the array
+      expect(followers).to.have.length.above(1);
+      // elements in the array should be a string
+      followers.forEach(function(follower) {
+        expect(follower).to.be.a("string");
+      })
+      done();
+    })
+  })
+})
+
+// We import a getUserFollows function from index.js
+// The function takes a git username & a callback function
+// we test that the array of folowers has at least 1 follower and is composed of strings
